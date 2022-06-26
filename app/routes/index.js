a4435236d607256432d593ad47962c2a2e31d73e
James Lee <48775473+titanian229@users.noreply.github.com>
2022-06-26 18:22:47 -0400

    Splitting endpoints into new lambda functions, testing weather endpoint

---
+++ app/routes/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { Duration, aws_lambda } = require("aws-cdk-lib");
+
+exports.routes = [
+  {
+    function: {
+      description: "Weather API endpoint",
+      memorySize: 256,
+      timeout: Duration.seconds(15),
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      entry: path.resolve("./weather.js"),
+      handler: "handler",
+    },
+    endpoint: "weather",
+    method: "GET",
+  },
+];
--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,10 +1,8 @@
-// const AWS = require("aws-sdk");
-
 exports.handler = async (/** @type {any} */ event) => {
   console.log("It worked!");
   // console.log("Event: ", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
-    body: "It worked!",
+    body: "It worked woo!",
   };
 };
